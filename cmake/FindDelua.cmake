c404b24734b80146862aebffe6344407994cbea2
George <juergen.sawinski@caesar.de>
2018-08-05 22:22:37 +0200

    Improved cmake configuration

--- cmake/FindDelua.cmake
+++ cmake/FindDelua.cmake
@@ -1,9 +1,9 @@
-message("CMAKE_MODULE_PATH = ${CMAKE_MODULE_PATH}")
 include(DeluaConfig
     OPTIONAL RESULT_VARIABLE DeluaConfig_FOUND)
 
 if(NOT DeluaConfig_FOUND)
     message("DeluaConfig.cmake not found.")
+    # FIXME load standard lua
 endif(NOT DeluaConfig_FOUND)
 
 
