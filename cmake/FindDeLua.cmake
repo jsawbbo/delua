cb4781fbad4de79b16d9536951f294034b30b2e2
George <juergen.sawinski@caesar.de>
2019-05-16 16:00:25 +0200

    Fix exported library names (and more).

--- cmake/FindDeLua.cmake
+++ cmake/FindDeLua.cmake
@@ -1,52 +1,13 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
-#
-# This file is partially copied from FindLua.cmake.
 
-#.rst:
-# FindDelua
-# ---------
-#
-#
-#
-# Locate Lua library built using Delua (https://bitbucket.org/jsaw/delua).
-# This module defines
-#
-# ::
-#
-#
-#   LUA_FOUND          - if false, do not try to link to Lua
-#   LUA_LIBRARIES      - both lua and lualib
-#   LUA_INCLUDE_DIR    - where to find lua.h
-#   LUA_VERSION_STRING - the version of Lua found
-#   LUA_VERSION_MAJOR  - the major version of Lua
-#   LUA_VERSION_MINOR  - the minor version of Lua
-#   LUA_VERSION_PATCH  - the patch version of Lua
-#
-#
-#
-# Note that the expected include convention is
-#
-# ::
-#
-#   #include "lua.h"
-#
-# and not
-#
-# ::
-#
-#   #include <lua/lua.h>
-#
-# This is because, the lua location is not standardized and may exist in
-# locations other than lua/
-
-if(Delua_FIND_VERSION_MAJOR AND NOT Delua_FIND_VERSION_MINOR)
+if(DeLua_FIND_VERSION_MAJOR AND NOT DeLua_FIND_VERSION_MINOR)
     message("Version major and minor are required.")
 endif()
 
-if(Delua_FIND_VERSION_MAJOR AND Delua_FIND_VERSION_PATCH)
+if(DeLua_FIND_VERSION_MAJOR AND DeLua_FIND_VERSION_PATCH)
     message(AUTHOR_WARNING "Patch version ignored.")
 endif()
 
-include(DeluaConfig-${Delua_FIND_VERSION_MAJOR}.${Delua_FIND_VERSION_MINOR}
-    OPTIONAL RESULT_VARIABLE DeluaConfig_FOUND)
+include(DeLuaConfig-${DeLua_FIND_VERSION_MAJOR}.${DeLua_FIND_VERSION_MINOR}
+    OPTIONAL RESULT_VARIABLE DeLuaConfig_FOUND)
--- lua/src/lua.hpp
+++ lua/src/lua.hpp
@@ -2,8 +2,14 @@
 // Lua header files for C++
 // <<extern "C">> not supplied automatically because Lua also compiles as C++
 
+#if !defined(DELUA_LANGUAGE_CXX)
 extern "C" {
+#endif
+
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
+
+#if !defined(DELUA_LANGUAGE_CXX)
 }
+#endif
