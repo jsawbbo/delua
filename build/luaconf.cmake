77b70973cda0985f7bcc787c09284e1729ae011b
George <juergen.sawinski@caesar.de>
2019-05-17 11:54:30 +0200

    LUA_ROOT must follow CMAKE_INSTALL_PREFIX.

--- build/luaconf.cmake
+++ build/luaconf.cmake
@@ -7,8 +7,13 @@ option(LUA_USE_DLOPEN "Use dlopen (requires dl library, auto-detected)." ${LUA_U
 option(LUA_USE_READLINE "Use readline features (requires readline library, auto-detected)." ${LUA_USE_READLINE_INIT})
 
 set(LUA_ROOT "${LUA_ROOT_INIT}" CACHE STRING "Root installation path.")
+
 if(NOT "${LUA_ROOT}" STREQUAL "${CMAKE_INSTALL_PREFIX}/")
-	message(WARNING "LUA_ROOT is different to the installation prefix (${CMAKE_INSTALL_PREFIX}/).")
+	if("${CMAKE_INSTALL_PREFIX_LAST}/" STREQUAL "${LUA_ROOT}")
+		set(LUA_ROOT "${CMAKE_INSTALL_PREFIX}/" CACHE STRING "Root installation path." FORCE)
+	else()
+		message(WARNING "LUA_ROOT is different to the installation prefix (${CMAKE_INSTALL_PREFIX}/).")
+	endif()
 endif()
 
 set(LUA_VDIR "${DeLua_RELEASE}" CACHE STRING "Version folder.")
