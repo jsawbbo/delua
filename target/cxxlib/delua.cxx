94ef995026516fbeec2c094e757a743ae65e13df
Jürgen "George" Sawinski <juergen.sawinski@mpinb.mpg.de>
2026-03-27 12:29:32 +0100

    modified C++ exception (not sure)

--- target/cxxlib/delua.cxx
+++ target/cxxlib/delua.cxx
@@ -8,24 +8,3 @@ extern "C" {
 }
 #endif
 
-const char *lua_Exception::what() const noexcept
-{
-  if (extra_) return extra_;
-
-  switch (lua::status(status_)) {
-  case lua::status::ok:
-    return "ok";
-  case lua::status::yield:
-    return "thread yielded";
-  case lua::status::error_run:
-    return "runtime error";
-  case lua::status::syntax:
-    return "syntax error";
-  case lua::status::memory:
-    return "out of memory";
-  case lua::status::error:
-    return "error in garbage collector";
-  default:
-    return "unknown";
-  }
-}
